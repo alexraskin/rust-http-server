4736a8b00cc77601727750d5e992548b7230ae18
alexraskin <alexraskin@fastmail.fm>
2022-02-11 16:49:13 -0700

    add buffer

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,4 @@
+use std::io::Read;
 use std::net::TcpListener;
 
 pub struct Server {
@@ -14,6 +15,19 @@ impl Server {
 
         let listener = TcpListener::bind(&self.addr).unwrap();
 
-        loop {}
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            print!("Received a request: {}", String::from_utf8_lossy(&buffer))
+                        }
+                        Err(e) => print!("Failed to read from connection: {}", e),
+                    }
+                }
+                Err(e) => println!("Failed to establish a connection {}", e),
+            }
+        }
     }
 }
