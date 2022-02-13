93311e50f5462050da1e636ef6e01350488ab2c4
alexraskin <alexraskin@fastmail.fm>
2022-02-12 22:28:05 -0700

    new changes

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,5 @@
+use crate::http::Request;
+use std::convert::TryFrom;
 use std::io::Read;
 use std::net::TcpListener;
 
@@ -21,7 +23,14 @@ impl Server {
                     let mut buffer = [0; 1024];
                     match stream.read(&mut buffer) {
                         Ok(_) => {
-                            print!("Received a request: {}", String::from_utf8_lossy(&buffer))
+                            print!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {
+                                    dbg!(request);
+                                }
+                                Err(e) => print!("Failed to parse a request {}", e),
+                            }
                         }
                         Err(e) => print!("Failed to read from connection: {}", e),
                     }
